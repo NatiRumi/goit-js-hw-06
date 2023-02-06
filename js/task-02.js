653615af986890684a06d02dab250a3bff2c0812
NatiRumi <NatiRumiantseva@gmail.com>
2023-02-06 23:46:33 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const elems = ingredients.map(ingr => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingr;
+  console.log(itemEl);
+  return itemEl;
+});
+
+const ulEl = document.querySelector('#ingredients');
+ulEl.append(...elems);
